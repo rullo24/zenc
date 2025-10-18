36b80d4e350a7cde51e8742f6583281f4f0ee5b6
rullo24 <temp@temp.com>
2025-10-18 15:44:40 +0800

    [bug fix] - second sequential decryption caused dzenc to be appended to the end (rather than replacing the ezenc)

--- src/main.zig
+++ src/main.zig
@@ -243,7 +243,5 @@ pub fn main() !void {
     // ensure all CONFIDENTIAL buffers are destroyed
     cipher.secureDestoryAllArgs(.{&s_output_buf, &s_raw_buf}); 
 
-    // FIXME: multiple encryptions and then decryptions doesn't get back to original data
-
 }
 
--- src/saver.zig
+++ src/saver.zig
@@ -15,16 +15,46 @@ pub fn saveOutput(
     io_stdout_writer: *std.Io.Writer,
 ) !void {
 
+    if (p_args_obj.opt_enc_file_loc == null and p_args_obj.opt_dec_file_loc == null) return error.NULL_ENC_AND_DEC_FILE;
+
     // get basename of parsed file for adding enc or dec extension to
     const s_encdec_basename: []const u8 =
         if (p_args_obj.opt_enc_file_loc != null) std.fs.path.basename(p_args_obj.opt_enc_file_loc.?)
         else if (p_args_obj.opt_dec_file_loc != null) std.fs.path.basename(p_args_obj.opt_dec_file_loc.?) 
         else return error.ENC_OR_DEC_FILE_DNE;
 
-    // replacing last ".ezenc" --> ".dzenc" if decrypting
-    const s_encdec_basename_wo_last_ezenc: []const u8 = 
-        if (std.mem.eql(u8, std.fs.path.extension(s_encdec_basename), ".ezenc") and p_args_obj.opt_enc_file_loc == null) std.fs.path.stem(s_encdec_basename)
-        else s_encdec_basename;
+    // create buffer for new basename w/ replaced "ezenc"
+    const s_new_basename: []u8 = try alloc.alloc(u8, s_encdec_basename.len + (".dzenc".len));
+    defer alloc.free(s_new_basename);
+    for (s_new_basename) |*p_c| p_c.* = 0x0; // zero all bytes in slice just created
+    @memcpy(s_new_basename[0..s_encdec_basename.len], s_encdec_basename); // copy current basename to buffer
+    var basename_size: usize = s_encdec_basename.len; // mimics copied parent at start
+
+    // replace last ezenc w/ dzenc (if avail), otherwise concatenate dzenc to end of basename
+    if (p_args_obj.opt_enc_file_loc != null) {
+
+        @memmove(s_new_basename[s_encdec_basename.len..(s_encdec_basename.len + ".ezenc".len)], ".ezenc");
+        basename_size += ".ezenc".len; // add extra characters for concatenation
+
+    } else { // act on decryption logic
+
+        const opt_i_last_ezenc: ?usize = std.mem.lastIndexOf(u8, s_new_basename[0..basename_size], ".ezenc");
+        if (opt_i_last_ezenc) |i_last_ezenc| { // act when .ezenc IS IN basename
+            @memmove(s_new_basename[ i_last_ezenc..i_last_ezenc+(".ezenc".len) ], ".dzenc"); // in-position replacement
+        } else { // act when .ezenc IS NOT IN basename
+            @memmove(s_new_basename[s_encdec_basename.len..(s_encdec_basename.len + ".dzenc".len)], ".dzenc");
+            basename_size += ".dzenc".len; // add extra characters for concatenation
+        }
+
+    }
+
+    // // creating new basename w/ zenc extension for saved file
+    // const s_new_basename: []const u8 = 
+    //     if (p_args_obj.opt_enc_file_loc != null) try std.fmt.allocPrint(alloc, "{s}.ezenc", .{s_encdec_basename_wo_last_ezenc})
+    //     else if (p_args_obj.opt_dec_file_loc != null) try std.fmt.allocPrint(alloc, "{s}.dzenc", .{s_encdec_basename_wo_last_ezenc})
+    //     else return error.ENC_OR_DEC_FILE_DNE;
+    // defer alloc.free(s_new_basename);
+
 
     // get file directory from path
     const s_opt_encdec_file_dir_loc: ?[]const u8 = 
@@ -32,14 +62,7 @@ pub fn saveOutput(
         else if (p_args_obj.opt_dec_file_loc != null) std.fs.path.dirname(p_args_obj.opt_dec_file_loc.?) 
         else return error.ENC_OR_DEC_FILE_DNE;
     if (s_opt_encdec_file_dir_loc == null) return error.NULL_FILE_DIRECTORY_CANNOT_SAVE;
-
-    // creating new basename w/ zenc extension for saved file
-    const s_new_basename: []const u8 = 
-        if (p_args_obj.opt_enc_file_loc != null) try std.fmt.allocPrint(alloc, "{s}.ezenc", .{s_encdec_basename_wo_last_ezenc})
-        else if (p_args_obj.opt_dec_file_loc != null) try std.fmt.allocPrint(alloc, "{s}.dzenc", .{s_encdec_basename_wo_last_ezenc})
-        else return error.ENC_OR_DEC_FILE_DNE;
-    defer alloc.free(s_new_basename);
-
+ 
     // capturing new save location from parsed directory and filename
     const s_new_save_loc: []const u8 = 
         if (p_args_obj.opt_enc_file_loc != null) try std.fs.path.join(alloc, &[_][]const u8{s_opt_encdec_file_dir_loc.?, s_new_basename})
